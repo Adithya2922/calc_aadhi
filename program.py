cef05d99595ee1c5ec8ffe36b56a9086500a492b
Adithya2922 <101918914+Adithya2922@users.noreply.github.com>
2022-03-28 15:18:52 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,8 @@
+Python 2.7.6 (default, Nov 10 2013, 19:24:18) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num1=7
+num2=10
+addition=(num1+num2)
+print("First number :",num1)
+print("Second number :",num2)
+print("Addition : ",addition)
